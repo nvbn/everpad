0716b75d66d58fc1efe1599d719ed6c3f8ccf078
Vladimir Yakovlev <nvbn.rm@gmai.com>
2012-04-27 18:23:18 +0400

    Hack for 12.04

--- thrift/transport/THttpClient.py
+++ thrift/transport/THttpClient.py
@@ -24,6 +24,19 @@ import urlparse
 import httplib
 import warnings
 import socket
+import ssl
+
+def connect(self):
+  "Connect to a host on a given (SSL) port."
+  sock = socket.create_connection((self.host, self.port),
+    self.timeout, self.source_address)
+  if self._tunnel_host:
+    self.sock = sock
+    self._tunnel()
+  self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file, ssl_version=ssl.PROTOCOL_TLSv1)
+
+httplib.HTTPS._connection_class.connect = connect
+
 
 class THttpClient(TTransportBase):
 

d77a9394ca94ce45155d33a2b028ace746543120
Vladimir Yakovlev <nvbn.rm@gmail.com>
2012-09-20 22:36:15 +0400

    bump to 1.9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.8'
+version = '1.9'
 
 setup(name='everpad',
     version=version,
--- setup_for_usc.py
+++ setup_for_usc.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import sys
 import os
 
-version = '1.8'
+version = '1.9'
 
 def get_files():
     packages = find_packages(exclude=['tests'])

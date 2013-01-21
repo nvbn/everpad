4fb5c2244e4e5987a2f90a81f654c6f6aa72227f
Samuele Santi <redshadow@hackzine.org>
2013-01-21 19:14:17 +0100

    Added dependency list

--- setup.py
+++ setup.py
@@ -17,7 +17,13 @@ setup(name='everpad',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=True,
-    install_requires=[],
+    install_requires=[
+        "httplib2",
+        "oauth2",
+        "sqlalchemy",
+        "regex",
+        "keyring",
+    ],
     entry_points={
         'gui_scripts': [
             'everpad=everpad.pad.indicator:main'

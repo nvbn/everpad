703fab4473dbeed4f1fa2483d2ab76802c07031b
Jesus M. Castagnetto <jesus@castagnetto.com>
2013-03-03 20:15:22 -0500

    unfinished translation

---
+++ i18n/es.ts
@@ -0,0 +1,578 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>ContentEdit</name>
+    <message>
+        <source>Insert Checkbox</source>
+        <translation>Insertar Casilla</translation>
+    </message>
+    <message>
+        <source>Insert Link</source>
+        <translation>Insertar URL</translation>
+    </message>
+    <message>
+        <source>Insert Table</source>
+        <translation>Insertar Tabla</translation>
+    </message>
+    <message>
+        <source>Insert Image</source>
+        <translation>Insertar Imágen</translation>
+    </message>
+    <message>
+        <source>Change link</source>
+        <translation>Cambiar URL</translation>
+    </message>
+    <message>
+        <source>Remove link</source>
+        <translation>Remover URL</translation>
+    </message>
+    <message>
+        <source>Paste as Plain Text</source>
+        <translation>Pegar como Texto Simple</translation>
+    </message>
+    <message>
+        <source>Image Preferences</source>
+        <translation>Preferencias de Imágen</translation>
+    </message>
+    <message>
+        <source>Change table</source>
+        <translation>Cambiar tabla</translation>
+    </message>
+    <message>
+        <source>Everpad / Insert link</source>
+        <translation>Everpad / Insertar URL</translation>
+    </message>
+    <message>
+        <source>Press link address</source>
+        <translation>Presionar en el vínculo</translation>
+    </message>
+    <message>
+        <source>Everpad / Change link</source>
+        <translation>Everpad / Cambiar URL</translation>
+    </message>
+    <message>
+        <source>Press new link address</source>
+        <translation>Presionar en el nuevo URL</translation>
+    </message>
+    <message>
+        <source>Image Files (*.png *.jpg *.bmp *.gif)</source>
+        <translation>Archivos de Imágenes (*.png *.jpg *.bmp *.gif)</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <source>Everpad / Settings and Management</source>
+        <translation>Everpad / Configuraciones y Manejo</translation>
+    </message>
+    <message>
+        <source>Authorisation</source>
+        <translation>Autorización</translation>
+    </message>
+    <message>
+        <source>Authorise</source>
+        <translation>Autorizar</translation>
+    </message>
+    <message>
+        <source>Sync delay</source>
+        <translation>Retraso en sincronización</translation>
+    </message>
+    <message>
+        <source>Start with system</source>
+        <translation>Iniciar con el sistema</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Configuración</translation>
+    </message>
+    <message>
+        <source>Note font family</source>
+        <translation>Tipo de letra para la nota</translation>
+    </message>
+    <message>
+        <source>Note font size</source>
+        <translation>Tamaño de letra para la nota</translation>
+    </message>
+    <message>
+        <source>Black tray icon</source>
+        <translation>Icono de sistema negro</translation>
+    </message>
+    <message>
+        <source>Launcher progress bar</source>
+        <translation>Barra de progreso durante la carga</translation>
+    </message>
+    <message>
+        <source>Search on the home lens</source>
+        <translation>Buscar en el lente base (Dash)</translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation>Apariencia</translation>
+    </message>
+</context>
+<context>
+    <name>Editor</name>
+    <message>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <source>Close without saving</source>
+        <translation>Cerrar sin guardar</translation>
+    </message>
+    <message>
+        <source>Remove note</source>
+        <translation>Remover la nota</translation>
+    </message>
+    <message>
+        <source>Print note</source>
+        <translation>Imprimir la nota</translation>
+    </message>
+    <message>
+        <source>Email note</source>
+        <translation>Enviar por e-mail la nota</translation>
+    </message>
+    <message>
+        <source>Find</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <source>Pin note</source>
+        <translation>Inmobilizar la nota</translation>
+    </message>
+    <message>
+        <source>Note &quot;%s&quot; saved!</source>
+        <translation>Nota &quot;%s&quot; guardada!</translation>
+    </message>
+    <message>
+        <source>You are trying to delete a note</source>
+        <translation>Esta tratando de eliminar una nota</translation>
+    </message>
+    <message>
+        <source>Are you sure want to delete this note?</source>
+        <translation>Está seguro que quiere eliminar esta nota?</translation>
+    </message>
+    <message>
+        <source>Note &quot;%s&quot; deleted!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Close without Saving</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Are you sure want to close this note without saving?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Select notebook</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Note tags</source>
+        <translation>Notitie-sleutelwoorden</translation>
+    </message>
+    <message>
+        <source>Commas separated tags</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%d attached files, &lt;a href=&apos;#&apos;&gt;show&lt;/a&gt; / &lt;a href=&apos;#&apos;&gt; add another&lt;/a&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>This note has alternative versions: %s</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>toolBar</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Save and close</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Ctrl+p</source>
+        <translation>Ctrl+P</translation>
+    </message>
+    <message>
+        <source>Ctrl+w</source>
+        <translation>Ctrl+W</translation>
+    </message>
+    <message>
+        <source> alternative of: %s</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Everpad / %s</source>
+        <translation>Everpad / %s</translation>
+    </message>
+    <message>
+        <source>Everpad</source>
+        <translation>Everpad</translation>
+    </message>
+</context>
+<context>
+    <name>FindBar</name>
+    <message>
+        <source>Form</source>
+        <translation>Formulario</translation>
+    </message>
+    <message>
+        <source>Find:</source>
+        <translation>Buscar:</translation>
+    </message>
+    <message>
+        <source>Previous</source>
+        <translation>Previo</translation>
+    </message>
+    <message>
+        <source>Next</source>
+        <translation>Siguiente</translation>
+    </message>
+    <message>
+        <source>Highlight All</source>
+        <translation>Marcar Todos</translation>
+    </message>
+    <message>
+        <source>Match Case</source>
+        <translation>Coincidir con las mayúsculas y/o minúsculas empleadas</translation>
+    </message>
+</context>
+<context>
+    <name>ImageDialog</name>
+    <message>
+        <source>Everpad / Image Preferences</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Width</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Height</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Discard ratio</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Indicator</name>
+    <message>
+        <source>API version missmatch, please restart</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Restart everpad</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>All Notes</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Create Note</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Wait, first sync in progress</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Sync in progress</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Please perform first sync</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Last Sync: Just now</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Last Sync: %s min ago</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Last Sync: %s mins ago</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Settings and Management</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>New note</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>New note content</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>List</name>
+    <message>
+        <source>Title</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Last Updated</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Create new notebook</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Notebook &quot;%s&quot; created!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Rename notebook</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Notebook &quot;%s&quot; renamed!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>You are trying to delete a notebook</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Are you sure want to delete this notebook and its notes?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Notebook &quot;%s&quot; deleted!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>You are trying to delete a note</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Are you sure want to delete note &quot;%s&quot;?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Note &quot;%s&quot; deleted!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>All Notes</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Enter notebook name:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Notebook with this name already exist. Enter notebook name</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Create Notebook</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Notebook</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Create Note</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Everpad / All Notes</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Ctrl+n</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <source>Ctrl+Shift+n</source>
+        <translation>Ctrl+Shift+N</translation>
+    </message>
+    <message>
+        <source>Ctrl+q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+</context>
+<context>
+    <name>Management</name>
+    <message>
+        <source>%d minutes</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>One hour</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Manual</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Remove Authorisation</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Authorise</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Notebook</name>
+    <message>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Notebook name</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Contains 5 notes</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message>
+        <source>QT_LAYOUT_DIRECTION</source>
+        <translation>QT_LAYOUT_DIRECTION</translation>
+    </message>
+</context>
+<context>
+    <name>ResourceEdit</name>
+    <message>
+        <source>%d attached files: &lt;a href=&quot;show&quot;&gt;%s&lt;/a&gt; / &lt;a href=&quot;add&quot;&gt;%s&lt;/a&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>show</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>hide</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>add another</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Put to Content</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Remove Resource</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Save As</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Delete Resource</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Are you sure want to delete this resource?</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>TableInsertDialog</name>
+    <message>
+        <source>Everpad / Insert Table</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Columns:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Rows:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Width:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>% of page</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>pixels</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>100</source>
+        <translation>100</translation>
+    </message>
+    <message>
+        <source>2</source>
+        <translation>2</translation>
+    </message>
+</context>
+</TS>

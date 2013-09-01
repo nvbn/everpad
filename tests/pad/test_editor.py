a15a5c2bae89e83fe7e9d18d87bb9800e03b18a2
nvbn <nvbn.rm@gmail.com>
2013-09-01 07:47:16 +0400

    Test interface only if need

--- tests/pad/test_editor.py
+++ tests/pad/test_editor.py
@@ -1,7 +1,7 @@
 from .. import settings
-
 from mock import MagicMock
-from PySide.QtCore import QSettings, Signal, QUrl, QObject
+from PySide.QtCore import QSettings, Signal, QUrl
+from PySide.QtGui import QApplication
 from everpad.provider.service import ProviderService
 from everpad.provider.tools import get_db_session
 from everpad.basetypes import (
@@ -14,9 +14,10 @@ from everpad.pad.editor.content import set_links
 from datetime import datetime
 import unittest
 import sys
+import os
 
 
-class FakeApp(QObject):
+class FakeApp(QApplication):
     data_changed = Signal()
 
     def update(self, service):
@@ -25,6 +26,7 @@ class FakeApp(QObject):
 
 
 app = FakeApp(sys.argv)
+
 
 CONTENTS = [
     u"<ul><li>23</li><li>567</li></ul>",
@@ -54,103 +56,113 @@ SET_LINKS = [
 ]
 
 
-class EditorTestCase(unittest.TestCase):
-    def setUp(self):
-        self.service = ProviderService()
-        self.service._session = get_db_session()
-        models.Note.session = self.service._session
-        self.app = app
-        app.update(self.service)
-        notebook = Notebook.from_tuple(
-            self.service.create_notebook('test', None),
-        )
-        self.note = Note.from_tuple(self.service.create_note(Note(
-            id=NONE_ID,
-            title='New note',
-            content="New note content",
-            tags=[],
-            notebook=notebook.id,
-            created=NONE_VAL,
-            updated=NONE_VAL,
-            place='',
-        ).struct))
+if 'test_editor' in os.environ:
+    class EditorTestCase(unittest.TestCase):
+        def setUp(self):
+            self.service = ProviderService()
+            self.service._session = get_db_session()
+            models.Note.session = self.service._session
+            self.app = app
+            self.app.update(self.service)
+            notebook = Notebook.from_tuple(
+                self.service.create_notebook('test', None),
+            )
+            self.note = Note.from_tuple(self.service.create_note(Note(
+                id=NONE_ID,
+                title='New note',
+                content="New note content",
+                tags=[],
+                notebook=notebook.id,
+                created=NONE_VAL,
+                updated=NONE_VAL,
+                place='',
+            ).struct))
 
-    def test_content_nochange(self):
-        """Test content nochange"""
-        editor = Editor(self.note)
-        self.assertEqual(
-            editor.note_edit.content,
-            "New note content",
-        )
-        for content in CONTENTS:
-            editor.note_edit.content = content
+        def tearDown(self):
+            if hasattr(self, 'editor'):
+                self.app.data_changed.disconnect(
+                    self.editor.init_alternatives,
+                )
+            del self.app.provider
+            del self.app
+
+        def test_content_nochange(self):
+            """Test content nochange"""
+            self.editor = Editor(self.note)
             self.assertEqual(
-                editor.note_edit.content,
+                self.editor.note_edit.content,
+                "New note content",
+            )
+            for content in CONTENTS:
+                self.editor.note_edit.content = content
+                self.assertEqual(
+                    self.editor.note_edit.content,
+                    content,
+                )
+
+        def test_content_changing(self):
+            """Test content changing"""
+            self.editor = Editor(self.note)
+            for prev, current in CHANGING_CONTENTS:
+                self.editor.note_edit.content = prev
+                self.assertEqual(
+                    self.editor.note_edit.content,
+                    current,
+                )
+
+        def test_title_nochange(self):
+            """Test title nochange"""
+            self.editor = Editor(self.note)
+            self.assertEqual(
+                self.editor.note_edit.title,
+                "New note",
+            )
+            for title in TITLES:
+                self.editor.note_edit.title = title
+                self.assertEqual(
+                    self.editor.note_edit.title,
+                    title,
+                )
+
+        def test_set_links(self):
+            """Test set links"""
+            for orig, result in SET_LINKS:
+                self.assertEqual(
+                    set_links(orig), result,
+                )
+
+        def test_not_broken_note_links(self):
+            """Test content nochange"""
+            content = '<a href="evernote:///view/123/123/123/">note link</a>'
+            self.note.content = content
+            self.editor = Editor(self.note)
+            self.assertEqual(
+                self.editor.note_edit.content,
                 content,
             )
 
-    def test_content_changing(self):
-        """Test content changing"""
-        editor = Editor(self.note)
-        for prev, current in CHANGING_CONTENTS:
-            editor.note_edit.content = prev
-            self.assertEqual(
-                editor.note_edit.content,
-                current,
+        def test_open_note_links(self):
+            """Test open note links"""
+            guid = 'guid'
+            note = Note(
+                id=123,
             )
 
-    def test_title_nochange(self):
-        """Test title nochange"""
-        editor = Editor(self.note)
-        self.assertEqual(
-            editor.note_edit.title,
-            "New note",
-        )
-        for title in TITLES:
-            editor.note_edit.title = title
-            self.assertEqual(
-                editor.note_edit.title,
-                title,
+            self.app.open = MagicMock()
+            self.service.get_note_by_guid = MagicMock(
+                return_value=note.struct,
             )
 
-    def test_set_links(self):
-        """Test set links"""
-        for orig, result in SET_LINKS:
-            self.assertEqual(
-                set_links(orig), result,
+            link = "evernote:///view/123/123/{guid}/123/".format(
+                guid=guid,
             )
+            self.editor = Editor(self.note)
+            self.editor.note_edit.link_clicked(QUrl(link))
 
-    def test_not_broken_note_links(self):
-        """Test content nochange"""
-        content = '<a href="evernote:///view/123/123/123/">note link</a>'
-        self.note.content = content
-        editor = Editor(self.note)
-        self.assertEqual(
-            editor.note_edit.content,
-            content,
-        )
-
-    def test_open_note_links(self):
-        """Test open note links"""
-        guid = 'guid'
-        note = Note(
-            id=123,
-        )
-
-        self.app.open = MagicMock()
-        self.service.get_note_by_guid = MagicMock(
-            return_value=note.struct,
-        )
-
-        link = "evernote:///view/123/123/{guid}/123/".format(
-            guid=guid,
-        )
-        editor = Editor(self.note)
-        editor.note_edit.link_clicked(QUrl(link))
-
-        self.assertEqual(
-            self.service.get_note_by_guid.call_args[0][0], guid,
-        )
-        self.assertEqual(
-            self.app.open.call_args[0][0].id, note.id,
-        )
+            self.assertEqual(
+                self.service.get_note_by_guid.call_args[0][0], guid,
+            )
+            self.assertEqual(
+                self.app.open.call_args[0][0].id, note.id,
+            )
+            del self.app.open

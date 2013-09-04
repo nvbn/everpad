32bbc426845b3bfbde11ff695d3758dc692df450
nvbn <nvbn.rm@gmail.com>
2013-09-04 06:17:03 +0400

    Add local settings for ci

---
+++ tests/settings/ci_local.py
@@ -0,0 +1,5 @@
+HOST = 'sandbox.evernote.com'
+CONSUMER_KEY = 'nvbn-1422'
+CONSUMER_SECRET = 'c17c0979d0054310'
+DB_PATH = ':memory:'
+TOKEN = 'S=s1:U=6604d:E=145758f3852:C=13e1dde0c54:P=1cd:A=en-devtoken:V=2:H=d2b720cdb06c99a105f937e669ebfc67'
